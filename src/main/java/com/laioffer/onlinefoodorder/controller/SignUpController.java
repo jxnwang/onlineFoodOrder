8be8efa361847b0245524817918359beca87ac0d
jxnwang <89869339+jxnwang@users.noreply.github.com>
2022-04-02 12:51:14 -0500

    2

---
+++ src/main/java/com/laioffer/onlinefoodorder/controller/SignUpController.java
@@ -0,0 +1,20 @@
+package com.laioffer.onlinefoodorder.controller;
+
+import com.laioffer.onlinefoodorder.entity.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class SignUpController {
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void signUp(@RequestBody Customer customer) {
+        //reflection
+    }
+}
+
+
